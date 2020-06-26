012001a1b536c8af14be0980cceae3505d56ffca
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-06-26 21:31:20 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    ServoBit.setServo(0, -90)
+    ServoBit.ledClear()
+    basic.pause(100)
+    for (let index = -90; index <= 90; index++) {
+        ServoBit.moveServo(0, index, 1)
+        basic.pause(100)
+    }
+})
+ServoBit.ledClear()
+ServoBit.ledBrightness(12)
+ServoBit.startFlash(0x18E600, 100)
+ServoBit.centreServos()
